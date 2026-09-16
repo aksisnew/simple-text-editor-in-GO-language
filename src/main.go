536257fb7d521ce43050f05996cfac692ca9f52b
Akshun A <203590953+aksisnew@users.noreply.github.com>
2026-09-16 22:57:52 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -3,25 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Simple Go Text Editor")
-	fmt.Println("---------------------")
-	fmt.Println("1. Open file")
-	fmt.Println("2. Create new file")
-	fmt.Println("3. Exit")
-	fmt.Println()
+	for {
+		fmt.Println()
+		fmt.Println("Simple Go Text Editor")
+		fmt.Println("---------------------")
+		fmt.Println("1. Open file")
+		fmt.Println("2. Create new file")
+		fmt.Println("3. Save file")
+		fmt.Println("4. Change extension")
+		fmt.Println("5. Exit")
+		fmt.Println()
 
-	var choice int
-	fmt.Print("Choose an option: ")
-	fmt.Scanln(&choice)
+		var choice int
+		fmt.Print("Choose an option: ")
+		fmt.Scanln(&choice)
 
-	switch choice {
-	case 1:
-		openFile()
-	case 2:
-		newFile()
-	case 3:
-		exitEditor()
-	default:
-		fmt.Println("Invalid option.")
+		switch choice {
+		case 1:
+			openFile()
+		case 2:
+			newFile()
+		case 3:
+			saveFile()
+		case 4:
+			changeExtension()
+		case 5:
+			exitEditor()
+		default:
+			fmt.Println("Invalid option.")
+		}
 	}
 }
